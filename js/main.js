940dc523c344cbd44df4d4ebbb2fcdccfdf49142
Ivan Diniz <ivandinizaj@gmail.com>
2018-09-16 15:58:11 -0300

    update page inicial e final e update computacao grafica

---
+++ js/main.js
@@ -0,0 +1,12 @@
+
+var target = document.getElementById("target");
+
+function r(min,max) {
+  return Math.floor(Math.random()*(max-min+1)+min);
+}
+
+setInterval(function() {
+  target.style.left = r(49,51) + "%"
+  target.style.top = r(49,51) + "%"
+  target.style.color = "hsl("+r(0,360)+",100%, 70%)"
+}, 80);
