e436c08c9ba497a9fe5c4880d27c914987b33945
Ivan Diniz <ivandinizaj@gmail.com>
2018-09-19 01:22:04 -0300

    add cronometro

--- js/main.js
+++ js/main.js
@@ -9,3 +9,50 @@ setInterval(function() {
   target.style.top = r(49,51) + "%";
   target.style.color = "hsl("+r(0,360)+",100%, 70%)";
 }, 80);
+
+
+function disableselect(e){ 
+	return false;
+}
+
+function reEnable(){ 
+	return true;
+} 
+
+//if IE4+ 
+document.onselectstart=new Function ("return false");
+document.oncontextmenu=new Function ("return false") ;
+//if NS6 
+if (window.sidebar){ 
+	document.onmousedown=disableselect;
+	document.onclick=reEnable;
+}
+
+if(Reveal.getQueryHash().s){
+
+	var timer = new Timer();
+	var chrono = document.getElementById('chronoExample');
+	chrono.style.zIndex = 9999;
+	chrono.style.display = 'block';
+
+	document.querySelector('#chronoExample .startButton').addEventListener('click', function () {
+	    timer.start();
+	});
+	document.querySelector('#chronoExample .pauseButton').addEventListener('click', function () {
+	    timer.pause();
+	});
+	document.querySelector('#chronoExample .stopButton').addEventListener('click', function () {
+	    timer.reset();
+			timer.pause();
+	});
+
+	timer.addEventListener('secondsUpdated', function (e) {
+	    document.querySelector('#chronoExample .values').innerHTML = (timer.getTimeValues().toString());
+	});
+	timer.addEventListener('started', function (e) {
+	    document.querySelector('#chronoExample .values').innerHTML = (timer.getTimeValues().toString());
+	});
+	timer.addEventListener('reset', function (e) {
+	    document.querySelector('#chronoExample .values').innerHTML = (timer.getTimeValues().toString());
+	}); 
+}
